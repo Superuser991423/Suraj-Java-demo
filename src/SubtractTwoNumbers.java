69b7b19adcb9bbbac04bf5756d08c0ad6a79991b
MauryaSuraj <Suraj3801Maurya@gmail.com>
2025-09-24 09:59:13 +0530

    new commit

--- src/SubtractTwoNumbers.java
+++ src/SubtractTwoNumbers.java
@@ -1,4 +1,9 @@
 public class SubtractTwoNumbers {
     public static void main(String[] args) {
-    System.out.println("Hello and welcome!");
-}}
+        int num1 = 20;
+        int num2 = 10;
+        int result = num1 - num2;
+
+        System.out.println("The difference between " + num1 + " and " + num2 + " is: " + result);
+    }
+}
