4109b4333542c401bde82fcb9d424129511f0143
MauryaSuraj <Suraj3801Maurya@gmail.com>
2025-10-01 14:35:46 +0530

    new commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+// This is a simple Java program for beginners
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // This line prints text to the console
+        System.out.println("Suraj Maurya");
+    }
+}
